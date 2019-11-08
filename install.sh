2153fca0b9214ac3241ee83957e5dd67eab6e9fb
CLAYS HACKER <37822373+CLAYSHACKER@users.noreply.github.com>
2019-11-08 15:05:11 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# NINGUN SISTEMA ES SEGURO
+# Obfuscate by @Clays hacker
+# Channel: https://www.youtube.com/channel/UCDmwCTzunUqrQx6BxCAPkQQ
+#
+#
+CesarHackGray=$(mktemp)
+base64 -d  >${CesarHackGray}<<DIXIE
+IyEvdXNyL2Jpbi9lbnYgYmFzaAojCiMgT2JmdXNjYXRlIGJ5IEBDZXNhckhhY2tHcmF5CiMgQ29u
+dGFjdGEgaHR0cHM6Ly90Lm1lL0Nlc2FyR3JheQojIENoYW5uZWw6IGh0dHBzOi8vd3d3LnlvdXR1
+YmUuY29tL2NoYW5uZWwvVUNqczBOOFBiRW8tc2Uwcl80T19zdk5RCiMKIwpDZXNhckhhY2tHcmF5
+PSQobWt0ZW1wKQpiYXNlNjQgLWQgID4ke0Nlc2FySGFja0dyYXl9PDxESVhJRQo=
+DIXIE
+source ${CesarHackGray}
+rm -rf ${CesarHackGray}
